699a332daf5cc4067e3072c76dea9f700c888352
tangusjr <kipngenogilbert96@gmail.com>
2022-06-22 15:36:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+    for (let i = 0; i < word.length / 2; i++) {
+      // compare the letter we're iterating over to the corresponding letter at the end of the string
+      const lastIndex = word.length - 1 - i;
+      if (word[i] !== word[lastIndex]) {
+        return false;
+      }
+    }
+  
+    // if we reach the middle, and all the letters match, return true
+    return true;
 }
 
 /* 
